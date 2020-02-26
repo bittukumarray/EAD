29dfcb82aefcdba47c682c4b992a62eed3b24e1c
Aaquib Niaz <aaquibniaz3600@gmail.com>
2020-02-27 02:18:22 +0530

    updating login

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,5 @@
-import React, { Component, useEffect } from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import {
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarNav,
-  MDBNavbarToggler,
-  MDBCollapse,
-  MDBNavItem,
-  MDBFooter,
-  MDBNavLink,
-  MDBTooltip,
-  MDBIcon
-} from "mdbreact";
-// import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from "./Routes";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Register from "./components/auth/Register";
@@ -25,7 +11,7 @@ import Alert from "./components/layout/Alert";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
-import { loadUser, loadCompanyUser } from "./actions/auth";
+import { loadUser } from "./actions/auth";
 // import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 // import companyRegister from "./components/auth/companyRegister";
@@ -39,31 +25,26 @@ const App = () => {
     if (localStorage.token) {
       setAuthToken(localStorage.token);
       store.dispatch(loadUser());
-      // store.dispatch(loadCompanyUser());
     }
   }, []);
   return (
     <Provider store={store}>
       <Router>
-        <div className="flyout">
-          <Navbar />
+        {/* <div className="flyout"> */}
+        <Navbar />
 
-          <main style={{ marginTop: "4rem" }}>
-            <Alert />
+        <main style={{ marginTop: "4rem" }}>
+          <Alert />
 
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route
-                exact
-                path="/company-register"
-                component={CompanyRegister}
-              />
-              <Route exact path="/company-login" component={CompanyLogin} />
-              <Route exact path="/" component={Landing} />
-            </Switch>
-          </main>
-        </div>
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/company-register" component={CompanyRegister} />
+            <Route exact path="/company-login" component={CompanyLogin} />
+            <Route exact path="/" component={Landing} />
+          </Switch>
+        </main>
+        {/* </div> */}
       </Router>
     </Provider>
   );
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -8,7 +8,8 @@ import {
   MDBCardBody,
   MDBCardHeader,
   MDBIcon,
-  MDBBtn
+  MDBBtn,
+  MDBModalFooter
 } from "mdbreact";
 import SectionContainer from "../../components/sectionContainer";
 import { connect } from "react-redux";
@@ -51,52 +52,104 @@ const Login = ({ loginFarmer, isAuthenticated }) => {
 
   return (
     <Fragment>
-      <div className="container">
-        <MDBContainer>
-          <MDBRow>
-            <MDBCol md="6">
-              <MDBCard>
-                <MDBCardBody>
-                  <MDBCardHeader className="form-header deep-blue-gradient rounded">
-                    <h3 className="my-3">
-                      <MDBIcon icon="lock" /> Login:
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol md="6">
+            <MDBCard>
+              <MDBCardBody className="mx-4">
+                <form onSubmit={e => onSubmit(e)}>
+                  <div className="text-center">
+                    <h3 className="dark-grey-text mb-5">
+                      <strong>Sign in</strong>
                     </h3>
-                  </MDBCardHeader>
-                  <form onSubmit={e => onSubmit(e)}>
-                    <p className="h5 text-center mb-4">Sign inn</p>
-                    <div className="grey-text">
-                      <MDBInput
-                        label="Type your email"
-                        icon="lock"
-                        group
-                        name="email"
-                        // type="password"
-                        onChange={e => onChange(e)}
-                        validate
-                        // required
-                      />
+                  </div>
+                  <MDBInput
+                    label="Your email"
+                    group
+                    name="email"
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                    onChange={e => onChange(e)}
+                  />
 
-                      <MDBInput
-                        label="Type your password"
-                        icon="lock"
-                        group
-                        name="password"
-                        type="password"
-                        onChange={e => onChange(e)}
-                        validate
-                      />
-                    </div>
+                  <MDBInput
+                    label="Your password"
+                    group
+                    type="password"
+                    name="password"
+                    validate
+                    containerClass="mb-0"
+                    onChange={e => onChange(e)}
+                  />
 
-                    <div className="text-center">
+                  <p className="font-small blue-text d-flex justify-content-end pb-3">
+                    Forgot
+                    <a href="#!" className="blue-text ml-1">
+                      Password?
+                    </a>
+                  </p>
+                  <div className="text-center mb-3">
+                    <MDBBtn
+                      type="submit"
+                      gradient="blue"
+                      rounded
+                      className="btn-block z-depth-1a"
+                    >
+                      Sign in
+                    </MDBBtn>
+                    {/* <div className="text-center">
                       <MDBBtn type="submit">Login</MDBBtn>
-                    </div>
-                  </form>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </div>
+                    </div> */}
+                  </div>
+                  <p className="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2">
+                    or Sign in with:
+                  </p>
+                  <div className="row my-3 d-flex justify-content-center">
+                    <MDBBtn
+                      type="button"
+                      color="white"
+                      rounded
+                      className="mr-md-3 z-depth-1a"
+                    >
+                      <MDBIcon
+                        fab
+                        icon="facebook-f"
+                        className="blue-text text-center"
+                      />
+                    </MDBBtn>
+                    <MDBBtn
+                      type="button"
+                      color="white"
+                      rounded
+                      className="mr-md-3 z-depth-1a"
+                    >
+                      <MDBIcon fab icon="twitter" className="blue-text" />
+                    </MDBBtn>
+                    <MDBBtn
+                      type="button"
+                      color="white"
+                      rounded
+                      className="z-depth-1a"
+                    >
+                      <MDBIcon fab icon="google-plus-g" className="blue-text" />
+                    </MDBBtn>
+                  </div>
+                </form>
+              </MDBCardBody>
+              <MDBModalFooter className="mx-5 pt-3 mb-1">
+                <p className="font-small grey-text d-flex justify-content-end">
+                  Not a member?
+                  <a href="#!" className="blue-text ml-1">
+                    Sign Up
+                  </a>
+                </p>
+              </MDBModalFooter>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </Fragment>
   );
 };
