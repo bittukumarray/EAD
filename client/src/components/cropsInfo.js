dd79aa3480350417a7e494950a48e8654f12bbfe
rohit7s <sony8947947@gmail.com>
2020-03-02 19:57:52 +0530

    heading change

--- client/src/components/cropsInfo.js
+++ client/src/components/cropsInfo.js
@@ -18,14 +18,8 @@ class BlogPage extends Component {
             <MDBCard className="my-5 px-5 pb-5">
       <MDBCardBody className="text-left">
         <h2 className="h1-responsive font-weight-bold text-center my-5">
-          Recent posts
+          Information About Crops
         </h2>
-        <p className="text-center w-responsive mx-auto mb-5">
-          Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          cupidatat non proident, sunt in culpa qui officia deserunt mollit
-          anim id est laborum.
-        </p>
             {this.props.cropsInfo[0] ? this.props.cropsInfo[0].map((element)=>
             <MDBRow key={element._id}><MDBCol lg="2" md="12" className="mb-lg-0 mb-4">
             </MDBCol>
