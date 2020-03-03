76de684015ec2242a7e138e43dde2ba183e3093d
wilson <wilson.p17@iiits.in>
2020-03-03 21:24:17 +0530

    made the orders model

---
+++ backend/models/order.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.schema;
+
+const OrderSchema = new Schema({
+  crops: [
+    {
+      crop: { type: Object, required: true, ref: "Crop" },
+      quantity: { type: Number, required: true }
+    }
+  ],
+  user: {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User"
+    }
+  },
+  farmer: {
+    type: Schema.Types.ObjectId,
+    ref: "farmer"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
