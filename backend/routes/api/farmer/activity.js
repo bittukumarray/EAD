9f40cfe9ffe5abba64b4f70ee064d3f5a65a43d3
wilson <wilson.p17@iiits.in>
2020-03-05 15:51:57 +0530

    some changes

--- backend/routes/api/farmer/activity.js
+++ backend/routes/api/farmer/activity.js
@@ -12,10 +12,12 @@ router.get("/get-crops", async (req, res, next) => {
 });
 
 //get a single crop
-router.get("get-crop/:cropId", async (req, res, next) => {
+router.get("/get-crop/:cropId", async (req, res, next) => {
   const cropId = await Crops.findById(req.params.cropId);
+  console.log(cropId);
   res.status(200).json({ message: "Fetched a single crop", crop: cropId });
 });
+
 // crops adding api
 router.post("/add-crops", auth, async (req, res, next) => {
   const errors = validationResult(req);
