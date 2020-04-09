71238af992799cf379c99909bb122b8692959cb7
Aaquib Niaz <aaquibniaz3600@gmail.com>
2020-04-09 07:36:26 +0530

    navbar

--- client/src/components/crop/Crop.js
+++ client/src/components/crop/Crop.js
@@ -1,45 +1,44 @@
-import React, { Fragment, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {getCrop} from '../../actions/crops';
+import { getCrop } from "../../actions/crops";
 
-const Crop = ({ getCrop, crop: { crop,loading }, match }) => {
-    useEffect(() => {
+const Crop = ({ getCrop, crop: { crop, loading }, match }) => {
+  useEffect(() => {
     console.log("crop detail");
 
-      getCrop(match.params.id);
-      console.log(crop);
+    getCrop(match.params.id);
+    console.log(crop);
+  }, [getCrop, match.params.id]);
 
-    },[getCrop, match.params.id]);
-  
-    return loading || crop === null ? (
-        <div>loading</div>
-      ) : (
-        <Fragment>
-         name = {crop.name}
-         <hr/>
-         farmer = name {crop.farmer_name}
-         <hr/>
-         quantity : {crop.quantity}
-         <hr/>
-         price  = {crop.price}
-         <hr/>
-        city =  {crop.city}
-         <hr/>
-         details  = {crop.details}
-        </Fragment>
-      );
-  };
+  return loading || crop === null ? (
+    <div>loading</div>
+  ) : (
+    <Fragment>
+      name = {crop.name}
+      <hr />
+      farmer = name {crop.farmer_name}
+      <hr />
+      quantity : {crop.quantity}
+      <hr />
+      price = {crop.price}
+      <hr />
+      city = {crop.city}
+      <hr />
+      details = {crop.details}
+    </Fragment>
+  );
+};
 
 Crop.propTypes = {
-    getCrop: PropTypes.func.isRequired,
-    crop: PropTypes.object.isRequired
-  };
-  
-  const mapStateToProps = state => ({
-    crop: state.crop
-  });
+  getCrop: PropTypes.func.isRequired,
+  crop: PropTypes.object.isRequired,
+};
 
-  export default connect(mapStateToProps, { getCrop })(Crop);
+const mapStateToProps = (state) => ({
+  crop: state.crop,
+});
+
+export default connect(mapStateToProps, { getCrop })(Crop);
--- client/src/components/dashboard/topNavigation.js
+++ client/src/components/dashboard/topNavigation.js
@@ -1,58 +1,141 @@
-import React, { Component } from 'react';
-import { MDBNavbar, MDBMask, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBView } from 'mdbreact';
+import React, { Component } from "react";
+import {
+  MDBNavbar,
+  MDBMask,
+  MDBNavbarNav,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavItem,
+  MDBNavLink,
+  MDBView,
+} from "mdbreact";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-import './topNav.css';
+import "./topNav.css";
 
 class TopNavigation extends Component {
-    state = {
-        collapse: false
-    }
+  state = {
+    collapse: false,
+  };
 
-    onClick = () => {
-        this.setState({
-            collapse: !this.state.collapse,
-        });
-    }
+  onClick = () => {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  };
 
-    toggle = () => {
-        this.setState({
-            dropdownOpen: !this.state.dropdownOpen
-        });
-    }
-    render() {
-        return (
-            <MDBNavbar style={{height:"59px"}} className="flexible-navbar border navbar-dark success-color" light expand="md"  fixed="top">
-                <MDBNavbarToggler onClick = { this.onClick } />
-                <MDBCollapse isOpen = { this.state.collapse } navbar>
-                    <MDBNavbarNav left>
-                        <MDBNavItem active>
-                            <MDBNavLink to="#">Home</MDBNavLink>
-                        </MDBNavItem>
-                        <MDBNavItem className="nav-item">
-                            <a className="nav-link" rel="noopener noreferrer" className="nav-link Ripple-parent" href="#" target="_blank">Crops</a>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <a rel="noopener noreferrer" className="nav-link Ripple-parent" href="#" target="_blank">Sales</a>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <a rel="noopener noreferrer" className="nav-link Ripple-parent" href="#" target="_blank">Farmers</a>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <a rel="noopener noreferrer"  className="nav-link Ripple-parent" href="#" target="_blank">History</a>
-                        </MDBNavItem>
-                    </MDBNavbarNav>
-                    <MDBNavbarNav right>
-                        <MDBNavItem>
-                            <a href="#" target="_blank"><button type="button" class="btn btn-primary btn-md">Login</button></a>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <a href="#" target="_blank"><button type="button" class="btn btn-primary btn-md">Sign Up</button></a>
-                        </MDBNavItem>
-                    </MDBNavbarNav>
-                </MDBCollapse>
-            </MDBNavbar>
-        );
-    }
+  toggle = () => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  };
+  render() {
+    console.log(this.props.auth);
+    const { isAuthenticated } = this.props.auth;
+
+    const authLinks = (
+      <React.Fragment>
+        <MDBNavItem>
+          <a href="#" target="_blank">
+            <button type="button" class="btn btn-primary btn-md">
+              logout
+            </button>
+          </a>
+        </MDBNavItem>
+      </React.Fragment>
+    );
+
+    const guestLinks = (
+      <React.Fragment>
+        <MDBNavItem>
+          <a href="#" target="_blank">
+            <button type="button" class="btn btn-primary btn-md">
+              Login
+            </button>
+          </a>
+        </MDBNavItem>
+        <MDBNavItem>
+          <a href="#" target="_blank">
+            <button type="button" class="btn btn-primary btn-md">
+              signup
+            </button>
+          </a>
+        </MDBNavItem>
+      </React.Fragment>
+    );
+
+    return (
+      <MDBNavbar
+        style={{ height: "59px" }}
+        className="flexible-navbar border navbar-dark success-color"
+        light
+        expand="md"
+        fixed="top"
+      >
+        <MDBNavbarToggler onClick={this.onClick} />
+        <MDBCollapse isOpen={this.state.collapse} navbar>
+          <MDBNavbarNav left>
+            <MDBNavItem active>
+              <MDBNavLink to="#">Home</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem className="nav-item">
+              <a
+                className="nav-link"
+                rel="noopener noreferrer"
+                className="nav-link Ripple-parent"
+                href="#"
+                target="_blank"
+              >
+                Crops
+              </a>
+            </MDBNavItem>
+            <MDBNavItem>
+              <a
+                rel="noopener noreferrer"
+                className="nav-link Ripple-parent"
+                href="#"
+                target="_blank"
+              >
+                Sales
+              </a>
+            </MDBNavItem>
+            <MDBNavItem>
+              <a
+                rel="noopener noreferrer"
+                className="nav-link Ripple-parent"
+                href="#"
+                target="_blank"
+              >
+                Farmers
+              </a>
+            </MDBNavItem>
+            <MDBNavItem>
+              <a
+                rel="noopener noreferrer"
+                className="nav-link Ripple-parent"
+                href="#"
+                target="_blank"
+              >
+                History
+              </a>
+            </MDBNavItem>
+          </MDBNavbarNav>
+          <MDBNavbarNav right>
+            {isAuthenticated ? authLinks : guestLinks}
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+    );
+  }
 }
+TopNavigation.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
 
-export default TopNavigation;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, {})(TopNavigation);
