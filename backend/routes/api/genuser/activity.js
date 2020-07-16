9d2dc060bf5b242d97bb4302bbaf009e0d9e7854
bittukumarray <raybittu242@gmail.com>
2020-07-17 00:14:23 +0530

    add to cart and get cart api implemented

--- backend/routes/api/genuser/activity.js
+++ backend/routes/api/genuser/activity.js
@@ -8,24 +8,33 @@ const genUser = require("../../../models/genuser");
 const { GenUser } = require("../../../helpers/roles");
 const Farmer = require("../../../models/farmer");
 
-router.get("/get-cart",auth, async (req, res, next) => {
-  const cropId = await genUser.findById(req.user.id);
-  res.json(cropId.cart);
+router.get("/get-cart", auth, async (req, res, next) => {
+  try {
+    const cropId = await genUser.findById(req.user.id);
+    res.json(cropId.cart);
+  }
+  catch (e) {
+    res.status(500).json({ "msg": "something went wrong" });
+  }
 });
 
 router.post("/add-cart", auth, async (req, res, next) => {
-  const crop = await Crops.findById(req.body.cropsId);
-  const farmerData = await Farmer.findById(req.body.farmerId);
-  const userData = await genUser.findById(req.user.id);
-  const cartItem = {
-    productId: crop,
-    quantity: req.body.quantity,
-    farmerId: farmerData
+  try {
+    const crop = await Crops.findById(req.body.cropsId);
+    const farmerData = await Farmer.findById(req.body.farmerId);
+    const userData = await genUser.findById(req.user.id);
+    const cartItem = {
+      productId: crop,
+      quantity: req.body.quantity,
+      farmerId: farmerData
+    }
+    console.log('cart item is ', cartItem);
+    userData.cart.items.push(cartItem);
+    await userData.save();
+    res.status(200).json({ message: "items added to cart", cart: cartItem });
+  } catch (e) {
+    res.status(500).json({"msg":"Something went wrong"});
   }
-  console.log('cart item is ', cartItem);
-  userData.cart.items.push(cartItem);
-  await userData.save();
-  res.status(200).json({ message: "items added to cart", cart: cartItem });
 });
 
 router.post("/order", auth, async (req, res, next) => {
