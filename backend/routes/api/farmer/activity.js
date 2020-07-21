07fde1a482bd31e9788bb0a6694598506fb2e558
wilson <wilson.p17@iiits.in>
2020-07-21 21:13:43 +0530

    added the error check

--- backend/routes/api/farmer/activity.js
+++ backend/routes/api/farmer/activity.js
@@ -187,20 +187,28 @@ router.post("/get-farmer-crops", auth, async (req, res, next) => {
   }
 });
 router.post("/get-same-crops", async (req, res, next) => {
-  let crop = req.body.crop;
-  crop.toLowerCase();
-  crops = await Crops.find({ name: crop });
-  let finalcrop = [];
-  for (let i in crops) {
-    let dic = {};
-    let farmerId = crops[i].farmer._id;
-    console.log(farmerId);
-    farm = await User.findById(farmerId);
-    // console.log(farm);
-    dic["crops"] = crops[i];
-    dic["farmer"] = farm;
-    finalcrop.push(dic);
+  try {
+    let crop = req.body.crop;
+    crop.toLowerCase();
+    crops = await Crops.find({ name: crop });
+    let finalcrop = [];
+    for (let i in crops) {
+      let dic = {};
+      let farmerId = crops[i].farmer._id;
+      console.log(farmerId);
+      farm = await User.findById(farmerId);
+      // console.log(farm);
+      dic["crops"] = crops[i];
+      dic["farmer"] = farm;
+      finalcrop.push(dic);
+    }
+    return res.status(200).json({ finalcrop });
+  } catch (err) {
+    return res.status(400).json({
+      Type: "Failed",
+      Message: "Cannot fetch the crops",
+      errors: err
+    });
   }
-  return res.status(200).json({ finalcrop });
 });
 module.exports = router;
