56cae95972fdccc145ebe838203d25054eb8dac2
Aaquib Niaz <aaquibniaz3600@gmail.com>
2020-07-24 21:42:28 +0530

    upading farmer edit profile page

--- client/src/components/dashboard/pages/ProfilePage.js
+++ client/src/components/dashboard/pages/ProfilePage.js
@@ -69,114 +69,76 @@ class ProfilePage extends React.Component {
     if (error) return <div>Error Occured</div>;
     // console.log("this state", this.state);
     if (!user) return <div>Loading </div>;
-    return (
-      <div>
-        <div class="container py-2">
-          <div class="row my-2 ml-5 mt-5">
-            <div className="col-lg-4 text-center">
-              <h2
-                className="text-center font-weight-dark"
-                style={{ color: "#0099ff" }}
-              >
-                User Profile
-              </h2>
-            </div>
-            <div class="col-lg-8 mt-5"></div>
-            <div class="col-lg-8 order-lg-1 personal-info">
-              <form role="form" onSubmit={this.onSubmit}>
-                <div class="form-group row">
-                  <label
-                    class="col-lg-3 col-form-label form-control-label"
-                    style={{ fontSize: "20px" }}
-                  >
-                    Name :{" "}
-                    <input
-                      type="text"
-                      name="name"
-                      value={name}
-                      onChange={this.onChange}
-                    />
-                  </label>
-                </div>
 
-                <div class="form-group row">
-                  <label
-                    class="col-lg-3 col-form-label form-control-label"
-                    style={{ fontSize: "20px" }}
-                  >
-                    Email :{" "}
-                    <input
-                      type="email"
-                      name="email"
-                      value={email}
-                      onChange={this.onChange}
-                      disabled
-                    />
-                  </label>
-                </div>
-                <div class="form-group row">
-                  <label
-                    class="col-lg-3 col-form-label form-control-label"
-                    style={{ fontSize: "20px" }}
-                  >
-                    city :{" "}
-                    <input
-                      type="text"
-                      name="city"
-                      value={city}
-                      onChange={this.onChange}
-                    />
-                  </label>
-                </div>
-                <button type="submit">Submit</button>
-              </form>
-
-              {/* <div class="form-group row">
-                <div class="col-lg-9 ml-auto text-right">
-                  <input
-                    type="reset"
-                    class="btn btn-danger"
-                    value="Cancel"
-                  ></input>
-                  <input
-                    type="button"
-                    class="btn btn-primary"
-                    value="Save Changes"
-                  />
-                </div>
-              </div> */}
-            </div>
-
-            <div class="col-lg-4 order-lg-0 text-center">
-              <img
-                src="https://img.etimg.com/thumb/width-640,height-480,imgsize-120883,resizemode-1,msid-65900138/small-and-marginal-farmers-are-to-see-better-days-with-a-free-of-cost-farmer-to-farmer-rental-program-through-this-revolutionary-app-by-tafes-jfarm-services.jpg"
-                class="mx-auto img-fluid rounded-circle"
-                alt="Image ka kya krega"
-                style={{ width: "280px" }}
+    const form_elemnt = (
+      <form role="form" onSubmit={this.onSubmit} className="">
+        <div className="form-group ">
+          <label
+            className="col-lg-7"
+            // style={{ fontSize: "20px" }}
+          >
+            Name :{" "}
+            <div class="md-form form-lg">
+              <input
+                type="text"
+                name="name"
+                id="inputLGEx"
+                value={name}
+                onChange={this.onChange}
+                className="form-control form-control-lg"
               />
-              {/* <h6 class="my-4">Upload a new photo</h6>
-            <div class="input-group px-xl-4">
-              <form>
-                <div style={{ textAlign: "left" }} class="custom-file">
-                  <input
-                    type="file"
-                    class="custom-file-input"
-                    id="customFile"
-                  ></input>
-                  <label class="custom-file-label" for="customFile">
-                    Choose file
-                  </label>
-                </div>
-              </form>
-              <div class="input-group-append">
-                <button style={{ float: "right" }} class="btn btn-secondary">
-                  <i class="fa fa-upload"></i>
-                </button>
-              </div>
-            </div> */}
             </div>
-          </div>
+          </label>
         </div>
+
+        <div className="form-group row">
+          <label
+            className="col-lg-7"
+            // style={{ fontSize: "20px" }}
+          >
+            Email :{" "}
+            <div class="md-form form-lg">
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.onChange}
+                disabled
+                id="inputLGEx"
+                className="form-control form-control-lg"
+              />
+            </div>
+          </label>
+        </div>
+        <div className="form-group row">
+          <label className="col-lg-7">
+            city :{" "}
+            <div class="md-form form-lg">
+              <input
+                type="text"
+                name="city"
+                value={city}
+                onChange={this.onChange}
+                className="form-control form-control-lg"
+              />
+            </div>
+          </label>
+        </div>
+
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    );
+    return (
+      <div class="container py-2">
+        <img
+          src="https://img.etimg.com/thumb/width-640,height-480,imgsize-120883,resizemode-1,msid-65900138/small-and-marginal-farmers-are-to-see-better-days-with-a-free-of-cost-farmer-to-farmer-rental-program-through-this-revolutionary-app-by-tafes-jfarm-services.jpg"
+          class="mx-auto img-fluid rounded-circle"
+          alt="Image ka kya krega"
+          style={{ width: "280px" }}
+        />
+        {form_elemnt}
       </div>
     );
   }
